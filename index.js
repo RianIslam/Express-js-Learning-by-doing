1c1d105f6110d0d099d731f169c0cbfa976dcfc2
RianIslam <rianislamdr@gmail.com>
2021-03-23 03:10:29 +0600

    user/:34

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ app.get('/fruit',(req,res)=>{
     res.send({fruit:'banana',quantity:2999,price:20000});
 })
 
+app.get('/users/:id',(req,res)=>{
+    console.log(req.params);
+})
+
 app.listen(3001,() => console.log('Listening to port 3001'));
