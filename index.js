924c88b99d6964f996021ab735d47e8771cafadb
RianIslam <rianislamdr@gmail.com>
2021-03-23 03:03:01 +0600

    add json

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ app.get('/',(req,res)=>{
     res.send(fruit);
 })
 
+app.get('/fruit',(req,res)=>{
+    res.send({fruit:'banana',quantity:2999,price:20000});
+})
+
 app.listen(3001,() => console.log('Listening to port 3001'));
